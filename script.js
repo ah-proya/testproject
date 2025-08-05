83fe18388194a1398cb25abc24ade89678ce9323
ah-proya <ahproya@gmail.com>
2025-08-05 11:45:33 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    const heading = document.getElementById("heading");
+    heading.textContent = "متن با موفقیت تغییر کرد!";
+    heading.style.color = "#28a745";
+});
